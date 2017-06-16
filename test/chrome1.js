bac622bf766633e94d798ac67882636adfdf3edb
miluwu <miluwu@tencent.com>
2017-06-16 11:43:29 +0800

    new

--- test/chrome1.js
+++ test/chrome1.js
@@ -1,5 +1,5 @@
 var exec = require('child_process').exec
 
-exec('chrome --headless http://qq.com',function (cb,error) {
+exec('chrome --headless --disable-gpu --dump-dom http://kg.qq.com',function (cb,error) {
 	console.log(cb,error)
 })
